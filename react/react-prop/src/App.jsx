f634cb310430a1e32137577ba9020da73eda57a0
sourav <jangidsourav97@gmail.com>
2024-10-18 10:18:31 +0530

    d

--- react/react-prop/src/App.jsx
+++ react/react-prop/src/App.jsx
@@ -1,14 +1,36 @@
+import PropTypes from "prop-types"
 function App(props) {
-
-  let si = (props.data[0] * props.data[1] * props.data[2])/100
+  // let sum = 0;
+  // let si = (props.data[0] * props.data[1] * props.data[2])/100
+  // let newData = props.data.filter((n)=>n<90)
 
   return (
 
     <>
-    <p>done</p>
+    {/* <p>done</p>
+    {newData.map((n,i)=>{
+      <h1 key={i} >{n}</h1>
+      {sum += n}
+    })}
     <h1>{si}</h1>
+    <h2>{sum}</h2> */}
+    {/* 15-10-2024 */}
+
+{/*     
+    <h1>15-10-2024</h1>
+    {props.name}
+    {props.pass}
+    {String(props.isMarried)}
+    {props.children} */}
+    
     </>
   )
 }
-
+App.PropTypes = {
+  name: PropTypes.string,
+  pass: PropTypes.string,
+  isMarried: PropTypes.bool,
+  children: PropTypes.node,
+}
 export default App
+
--- react/react-prop/src/main.jsx
+++ react/react-prop/src/main.jsx
@@ -2,13 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-let data=[2,4,7]
-let dat=[2,4,8]
+// let data=[2,4,7]
+// let dat=[2,4,8]
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App data={data} />
-    <App data={dat} />
+    {/* <App data={data} />
+    <App data={dat} /> */}
+    {/* 15-10-2024 */}
 
-
+    {/* <App name='root' pass='root' isMarried={false} >
+      <button>add someting</button>
+      </App>
+    <App name='root' pass='root' isMarried={false} >
+      <p>paragraph</p>
+      </App>
+    <App name='root' pass={34} isMarried={false} >
+      <p>paragraph</p>
+      </App> */}
+      <App/>
   </StrictMode>,
 )
